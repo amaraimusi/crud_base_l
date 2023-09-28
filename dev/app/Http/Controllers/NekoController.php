c719b7ea6cdb53b4593600f7344c38ac23f72651
amaraimusi <amaraimusi@gmail.com>
2023-09-28 21:07:50 +0900

    DBツール

--- dev/app/Http/Controllers/NekoController.php
+++ dev/app/Http/Controllers/NekoController.php
@@ -8,7 +8,13 @@ use App\Models\Neko;
 use CrudBase\CrudBase;
 use App\Consts\ConstCrudBase;
 
-
+/**
+ * ネコ管理画面
+ * @since 2023-9-28
+ * @version 1.0.0
+ * @author amaraimusi
+ *
+ */
 class NekoController extends CrudBaseController{
 	
 	// 画面のバージョン → 開発者はこの画面を修正したらバージョンを変更すること。バージョンを変更するとキャッシュやセッションのクリアが自動的に行われます。
@@ -41,32 +47,33 @@ class NekoController extends CrudBaseController{
 		// リクエストのパラメータが空でない、または新バージョンフラグがONである場合、リクエストから検索データを受け取る
 		if(!empty($request->all()) || $new_version == 1){
 			$searches = [
-				'main_search' => $request->main_search, // メイン検索
+					'main_search' => $request->main_search, // メイン検索
 				
-				// CBBXS-3000
-				'id' => $request->id, // id
-				'neko_val' => $request->neko_val, // neko_val
-				'neko_name' => $request->neko_name, // neko_name
-				'neko_date' => $request->neko_date, // neko_date
-				'neko_type' => $request->neko_type, // 猫種別
-				'neko_dt' => $request->neko_dt, // neko_dt
-				'neko_flg' => $request->neko_flg, // ネコフラグ
-				'img_fn' => $request->img_fn, // 画像ファイル名
-				'note' => $request->note, // 備考
-				'sort_no' => $request->sort_no, // 順番
-				'delete_flg' => $request->delete_flg, // 無効フラグ
-				'update_user_id' => $request->update_user_id, // 更新者
-				'ip_addr' => $request->ip_addr, // IPアドレス
-				'created_at' => $request->created_at, // 生成日時
-				'updated_at' => $request->updated_at, // 更新日
-
-				// CBBXE
-				
-				'update_user' => $request->update_user, // 更新者
-				'page' => $request->sort, // ページ番号
-				'sort' => $request->sort, // 並びフィールド
-				'desc' => $request->desc, // 並び向き
-				'per_page' => $request->per_page, // 行制限数
+					'id' => $request->id, // id
+					
+					// CBBXS-6000
+					'neko_val' => $request->neko_val, // neko_val
+					'neko_name' => $request->neko_name, // neko_name
+					'neko_date' => $request->neko_date, // neko_date
+					'neko_type' => $request->neko_type, // 猫種別
+					'neko_dt' => $request->neko_dt, // neko_dt
+					'neko_flg' => $request->neko_flg, // ネコフラグ
+					'img_fn' => $request->img_fn, // 画像ファイル名
+					'note' => $request->note, // 備考
+					// CBBXE
+					
+					'sort_no' => $request->sort_no, // 順番
+					'delete_flg' => $request->delete_flg, // 無効フラグ
+					'update_user_id' => $request->update_user_id, // 更新者
+					'ip_addr' => $request->ip_addr, // IPアドレス
+					'created_at' => $request->created_at, // 生成日時
+					'updated_at' => $request->updated_at, // 更新日
+	
+					'update_user' => $request->update_user, // 更新者
+					'page' => $request->sort, // ページ番号
+					'sort' => $request->sort, // 並びフィールド
+					'desc' => $request->desc, // 並び向き
+					'per_page' => $request->per_page, // 行制限数
 			];
 			
 		}else{
--- dev/app/Http/Controllers/NekoTypeController.php
+++ dev/app/Http/Controllers/NekoTypeController.php
@@ -2,15 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Consts\crud_base_function;
 use Illuminate\Http\Request;
 use App\Models\NekoType;
+use CrudBase\CrudBase;
+use App\Consts\ConstCrudBase;
 
-
+/**
+ * ネコ種別管理画面
+ * @since 2023-9-28
+ * @version 1.0.0
+ * @author amaraimusi
+ *
+ */
 class NekoTypeController extends CrudBaseController{
 	
 	// 画面のバージョン → 開発者はこの画面を修正したらバージョンを変更すること。バージョンを変更するとキャッシュやセッションのクリアが自動的に行われます。
-	public $this_page_version = '1.0.1';
+	public $this_page_version = '1.0.0';
 	
 	/**
 	 * indexページのアクション
@@ -22,7 +30,7 @@ class NekoTypeController extends CrudBaseController{
 
 		// ログアウトになっていたらログイン画面にリダイレクト
 		if(\Auth::id() == null) return redirect('login');
-
+		
 		// 検索データのバリデーション
 		$validated = $request->validate([
 			'id' => 'nullable|numeric',
@@ -31,7 +39,7 @@ class NekoTypeController extends CrudBaseController{
 		
 		$sesSearches = session('neko_type_searches_key');// セッションからセッション検索データを受け取る
 
-		// セッション検索データの画面から旧画面バージョンを受け取る
+		// 新バージョンチェック  0:バージョン変更なし（通常）, 1:新しいバージョン
 		$new_version = $this->judgeNewVersion($sesSearches, $this->this_page_version);
 
 		$searches = []; // 検索データ
@@ -39,24 +47,27 @@ class NekoTypeController extends CrudBaseController{
 		// リクエストのパラメータが空でない、または新バージョンフラグがONである場合、リクエストから検索データを受け取る
 		if(!empty($request->all()) || $new_version == 1){
 			$searches = [
-				'main_search' => $request->main_search, // メイン検索
+					'main_search' => $request->main_search, // メイン検索
 				
-				// CBBXS-3000
-				'id' => $request->id, // id
-				'neko_type_name' => $request->neko_type_name, // ネコ種別
-				'sort_no' => $request->sort_no, // 順番
-				'delete_flg' => $request->delete_flg, // 無効フラグ
-				'update_user_id' => $request->update_user_id, // 更新ユーザーID
-				'ip_addr' => $request->ip_addr, // IPアドレス
-				'created_at' => $request->created_at, // 生成日時
-				'updated_at' => $request->updated_at, // 更新日
+					'id' => $request->id, // id
+					
+					// CBBXS-6000
+					'neko_type_name' => $request->neko_type_name, // ネコ種別
 
-				// CBBXE
-			    
-			    'update_user' => $request->update_user, // 更新者
-				'sort' => $request->sort, // 並びフィールド
-				'desc' => $request->desc, // 並び向き
-				'per_page' => $request->per_page, // 行制限数
+					// CBBXE
+					
+					'sort_no' => $request->sort_no, // 順番
+					'delete_flg' => $request->delete_flg, // 無効フラグ
+					'update_user_id' => $request->update_user_id, // 更新者
+					'ip_addr' => $request->ip_addr, // IPアドレス
+					'created_at' => $request->created_at, // 生成日時
+					'updated_at' => $request->updated_at, // 更新日
+	
+					'update_user' => $request->update_user, // 更新者
+					'page' => $request->sort, // ページ番号
+					'sort' => $request->sort, // 並びフィールド
+					'desc' => $request->desc, // 並び向き
+					'per_page' => $request->per_page, // 行制限数
 			];
 			
 		}else{
@@ -69,17 +80,120 @@ class NekoTypeController extends CrudBaseController{
 		session(['neko_type_searches_key' => $searches]); // セッションに検索データを書き込む
 
 		$userInfo = $this->getUserInfo(); // ログインユーザーのユーザー情報を取得する
+		$paths = $this->getPaths(); // パス情報を取得する
+		$def_per_page = 20; // デフォルト制限行数
 		
 		$model = new NekoType();
-		$data = $model->getData($searches);
+		$fieldData = $model->getFieldData();
+		$data = $model->getData($searches, ['def_per_page' => $def_per_page]);
+		$data_count = $data->total(); //　LIMIT制限を受けていないデータ件数
+		
+		// CBBXS-3020
 
-	   return view('neko_type.index', [
-			'data'=>$data,
-			'searches'=>$searches,
-			'userInfo'=>$userInfo,
-			'this_page_version'=>$this->this_page_version,
+        // CBBXE
+        
+		$crudBaseData = [
+				'list_data'=>$data,
+				'data_count'=>$data_count,
+				'searches'=>$searches,
+				'userInfo'=>$userInfo,
+				'paths'=>$paths,
+				'fieldData'=>$fieldData,
+				'model_name_c'=>'NekoType', // モデル名（キャメル記法）
+				'model_name_s'=>'neko_type', // モデル名（スネーク記法）
+				'def_per_page'=>$def_per_page, // デフォルト制限行数
+				'this_page_version'=>$this->this_page_version,
+				'new_version' => $new_version,
+				
+				// CBBXS-3020B
+
+				// CBBXE
+		];
+        
+		return view('neko_type.index', [
+			    'data'=>$data,
+			    'searches'=>$searches,
+				'userInfo'=>$userInfo,
+				'fieldData'=>$fieldData,
+				'this_page_version'=>$this->this_page_version,
+				'crudBaseData'=>$crudBaseData,
+			    
+			    // CBBXS-3020B
+
+			    // CBBXE
+		    
+				
+				
 	   ]);
 		
+	}
+	
+	/**
+	 * SPA型・入力フォームの登録アクション | 新規入力アクション、編集更新アクション、複製入力アクションに対応しています。
+	 * @return string
+	 */
+	public function regAction(){
+		
+		// ログアウトになっていたらログイン画面にリダイレクト
+		if(\Auth::id() == null) return redirect('login');
+		
+		$json=$_POST['key1'];
+		
+		$res = json_decode($json,true);
+		
+		$ent = $res['ent'];
+
+		// IDフィールドです。 IDが空である場合、 新規入力アクションという扱いになります。なお、複製入力アクションは新規入力アクションに含まれます。
+		$id = !empty($ent['id']) ? $ent['id'] : null;
+		
+		// DBテーブルからDBフィールド情報を取得します。
+		$dbFieldData = $this->getDbFieldData('neko_type_types');
+		
+		// 値が空であればデフォルトをセットします。
+		$ent = $this->setDefalutToEmpty($ent, $dbFieldData);
+		
+		// モデルを生成します。 新規入力アクションは真っ新なモデルを生成しますが、編集更新アクションの場合は、行データが格納されたモデルを生成します。
+		$model = empty($id) ? new NekoType() : NekoType::find($id);
+		
+		$userInfo = $this->getUserInfo(); // ログインユーザーのユーザー情報を取得する
+		
+		
+
+		// CBBXS-XXXX
+
+		// CBBXE
+		
+		$model->delete_flg = 0;
+		$model->update_user_id = $userInfo['id'];
+		$model->ip_addr = $userInfo['ip_addr'];
+		$model->updated_at = date('Y-m-d H:i:s');
+		
+		
+		if(empty($id)){
+			$model->sort_no =$this->getNextSortNo('neko_type_types', 'asc');
+			$model->save(); // DBへ新規追加: 同時に$modelに新規追加した行のidがセットされる。
+			$ent['id'] = $model->id;
+		}else{
+			$model->update(); // DB更新
+		}
+		
+		// ▼ ファイルアップロード関連
+		$fileUploadK = CrudBase::factoryFileUploadK();
+		$front_img_fn = $ent['img_fn'];
+		$exist_img_fn = $model->img_fn;
+		$fRes = $fileUploadK->uploadForSpa('neko_type', $_FILES, $ent, 'img_fn', $front_img_fn, $exist_img_fn);
+		if($fRes['db_reg_flg']){
+			$model->img_fn = $fRes['reg_fp'];
+			$model->update(); // DB更新
+		}
+
+		$ent = $model->toArray();
+		
+		if(!empty($fRes['errs'])) $ent['errs'] = $fRes['errs'];
+		
+		$json = json_encode($ent, JSON_HEX_TAG | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_HEX_APOS);
+		
+		return $json;
 	}
 	
 	
@@ -94,11 +208,53 @@ class NekoTypeController extends CrudBaseController{
 		// ログアウトになっていたらログイン画面にリダイレクト
 		if(\Auth::id() == null) return redirect('login');
 		
+		$model = new NekoType();
+		
+		$copy_id = $request->id; // 複製元のid。空なら普通の新規入力になる
+		
+		$ent = $model->find($copy_id); // 複製元のエンティティを取得
+		
+		// 複製元のエンティティが空であれば、通常の新規入力になる。新規入力のデフォルト値をセットする。
+		if($ent==null){
+			$ent = $model->get();
+			// CBBXS-4002
+			$ent->neko_type_name = ''; // ネコ種別
+			$ent->sort_no = ''; // 順番
+			$ent->delete_flg = 0; // 無効フラグ
+			$ent->update_user_id = ''; // 更新ユーザーID
+			$ent->ip_addr = ''; // IPアドレス
+			$ent->created_at = ''; // 生成日時
+			$ent->updated_at = ''; // 更新日
+
+			// CBBXE
+		}
+		
+		if($ent->neko_type_dt == '0000-00-00 00:00:00') $ent->neko_type_dt = '';
+		
 		$userInfo = $this->getUserInfo(); // ログインユーザーのユーザー情報を取得する
+		$paths = $this->getPaths(); // パス情報を取得する
+		
+		// CBBXS-3037
+
+		// CBBXE
+		
+		$crudBaseData = [
+				'ent'=>$ent->toArray(),
+				'userInfo'=>$userInfo,
+				'paths'=>$paths,
+				'this_page_version'=>$this->this_page_version,
+				'neko_typeTypeList'=>$neko_typeTypeList,
+		];
 		
 		return view('neko_type.create', [
-			'userInfo'=>$userInfo,
-			'this_page_version'=>$this->this_page_version,
+				'ent'=>$ent,
+				'userInfo'=>$userInfo,
+				'this_page_version'=>$this->this_page_version,
+				'crudBaseData' => $crudBaseData,
+				
+		    	// CBBXS-3037B
+
+		    	// CBBXE
 			
 		]);
 		
@@ -113,10 +269,12 @@ class NekoTypeController extends CrudBaseController{
 	 */
 	public function store(Request $request){
 		
-		if(\Auth::id() == null) die;
+		if(\Auth::id() == null) die();
+		
+		$userInfo = $this->getUserInfo(); // ログインユーザーのユーザー情報を取得する
 
 		$request->validate([
-			// CBBXS-3030
+				// CBBXS-3030
 			'id' => 'nullable|numeric',
 	        'neko_type_name' => 'nullable|max:255',
 			'sort_no' => 'nullable|numeric',
@@ -126,7 +284,6 @@ class NekoTypeController extends CrudBaseController{
 			// CBBXE
 		]);
 		
-		$userInfo = $this->getUserInfo(); // ログインユーザーのユーザー情報を取得する
 		
 		$model = new NekoType();
 		// CBBXS-3032
@@ -138,8 +295,16 @@ class NekoTypeController extends CrudBaseController{
 		$model->delete_flg = 0;
 		$model->update_user_id = $userInfo['id'];
 		$model->ip_addr = $userInfo['ip_addr'];
+		
+		$model->save(); // DBへ新規追加と同時に$modelに新規追加した行のidがセットされる。
 
-		$model->save();
+		// ▼ ファイルアップロード関連
+		$fileUploadK = CrudBase::factoryFileUploadK();
+		$ent = $model->toArray();
+		$ent['img_fn_exist'] = $request->img_fn_exist; // 既存・画像ファイル名 img_fnの付属パラメータ
+		$model->img_fn = $fileUploadK->uploadForLaravelMpa('neko_type', $_FILES,  $ent, 'img_fn', 'img_fn_exist');
+
+		$model->update(); // ファイル名をモデルにセットしたのでモデルをDB更新する。
 		
 		return redirect('/neko_type');
 		
@@ -159,6 +324,7 @@ class NekoTypeController extends CrudBaseController{
 		
 		$model = new NekoType();
 		$userInfo = $this->getUserInfo(); // ログインユーザーのユーザー情報を取得する
+		$paths = $this->getPaths(); // パス情報を取得する
 		
 		$id = $request->id;
 		if(!is_numeric($id)){
@@ -167,12 +333,26 @@ class NekoTypeController extends CrudBaseController{
 		}
 		
 		$ent = NekoType::find($id);
+		
+		// CBBXS-3037
+
+		// CBBXE
+		
+		$crudBaseData = [
+				'ent'=>$ent,
+				'userInfo'=>$userInfo,
+				'paths'=>$paths,
+				'this_page_version'=>$this->this_page_version,
+				'neko_typeTypeList'=>$neko_typeTypeList,
+		];
+		
 
 		return view('neko_type.show', [
-			'ent'=>$ent,
-			'userInfo'=>$userInfo,
-			'this_page_version'=>$this->this_page_version,
-			
+				'ent'=>$ent,
+				'userInfo'=>$userInfo,
+				'this_page_version'=>$this->this_page_version,
+				'neko_typeTypeList'=>$neko_typeTypeList,
+				'crudBaseData' => $crudBaseData,
 		]);
 		
 	}
@@ -191,6 +371,7 @@ class NekoTypeController extends CrudBaseController{
 
 		$model = new NekoType();
 		$userInfo = $this->getUserInfo(); // ログインユーザーのユーザー情報を取得する
+		$paths = $this->getPaths(); // パス情報を取得する
 		
 		$id = $request->id;
 		if(!is_numeric($id)){
@@ -200,10 +381,27 @@ class NekoTypeController extends CrudBaseController{
 	
 		$ent = NekoType::find($id);
 		
+		// CBBXS-3038
+
+		// CBBXE
+		
+		$crudBaseData = [
+				'ent'=>$ent->toArray(),
+				'userInfo'=>$userInfo,
+				'paths'=>$paths,
+				'this_page_version'=>$this->this_page_version,
+				'neko_typeTypeList'=>$neko_typeTypeList,
+		];
+		
 		return view('neko_type.edit', [
-			'ent'=>$ent,
-			'userInfo'=>$userInfo,
-			'this_page_version'=>$this->this_page_version,
+				'ent'=>$ent,
+				'userInfo'=>$userInfo,
+				'this_page_version'=>$this->this_page_version,
+				'crudBaseData'=>$crudBaseData,
+				
+			    // CBBXS-3038B
+
+				// CBBXE
 			
 		]);
 		
@@ -245,8 +443,14 @@ class NekoTypeController extends CrudBaseController{
 		$model->delete_flg = 0;
 		$model->update_user_id = $userInfo['id'];
 		$model->ip_addr = $userInfo['ip_addr'];
-		
- 		$model->update();
+
+		// ▼ ファイルアップロード関連
+		$fileUploadK = CrudBase::factoryFileUploadK();
+		$ent = $model->toArray();
+		$ent['img_fn_exist'] = $request->img_fn_exist; // 既存・画像ファイル名 img_fnの付属パラメータ
+		$model->img_fn = $fileUploadK->uploadForLaravelMpa('neko_type', $_FILES,  $ent, 'img_fn', 'img_fn_exist');
+
+ 		$model->update(); // DB更新
 		
 		return redirect('/neko_type');
 		
@@ -353,7 +557,7 @@ class NekoTypeController extends CrudBaseController{
 		$searches = session('neko_type_searches_key');// セッションからセッション検索データを受け取る
 
 		$model = new NekoType();
-		$data = $model->getData($searches, 'csv');
+		$data = $model->getData($searches, ['use_type'=>'csv'] );
 		
 		// データ件数が0件ならCSVダウンロードを中断し、一覧画面にリダイレクトする。
 		$count = count($data);
@@ -387,6 +591,46 @@ class NekoTypeController extends CrudBaseController{
 		//CSVダウンロード
 		$this->csvOutput($fn, $data);
 
+	}
+
+	
+	/**
+	 * AJAX | 一覧のチェックボックス複数選択による一括処理
+	 * @return string
+	 */
+	public function ajax_pwms(){
+		
+		// ログアウトになっていたらログイン画面にリダイレクト
+		if(\Auth::id() == null) return redirect('login');
+		
+		$json_param=$_POST['key1'];
+		
+		$param=json_decode($json_param,true);//JSON文字を配列に戻す
+		
+		// IDリストを取得する
+		$ids = $param['ids'];
+
+		// アクション種別を取得する
+		$kind_no = $param['kind_no'];
+
+		// ユーザー情報を取得する
+		$userInfo = $this->getUserInfo();
+
+		$model = new NekoType();
+		
+		// アクション種別ごとに処理を分岐
+		switch ($kind_no){
+			case 10:
+				$model->switchDeleteFlg($ids, 0, $userInfo); // 有効化
+				break;
+			case 11:
+				$model->switchDeleteFlg($ids, 1 ,$userInfo); // 削除化(無効化）
+				break;
+			default:
+				return "'kind_no' is unknown value";
+		}
+		
+		return 'success';
 	}
 
 
