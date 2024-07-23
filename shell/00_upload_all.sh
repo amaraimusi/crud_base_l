f52a503c36b18b92d7bc1f028f9c0f81a698a3a6
amaraimusi <amaraimusi@gmail.com>
2024-07-23 22:58:15 +0900

    アップロードシェルを更新

---
+++ shell/00_upload_all.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/sh
+echo 'すべてソースコードをアップ'
+
+rsync -auvz --exclude='.env' ../dev amaraimusi@amaraimusi.sakura.ne.jp:www/mng/crud_base_l/
+
+rsync -auvz  ../dev/.env_p amaraimusi@amaraimusi.sakura.ne.jp:www/mng/crud_base_l/dev/.env
+
+echo "------------ アップロード完了"
+#cmd /k
---
+++ shell/01_upload.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/sh
+echo 'ソースコードを差分アップロードします。'
+
+rsync -auvz --exclude='.env' --exclude='vendor' --exclude='storage' --exclude='tmp' --exclude='node_modules' ../dev amaraimusi@amaraimusi.sakura.ne.jp:www/mng/crud_base_l/
+
+rsync -auvz  ../dev/.env_p amaraimusi@amaraimusi.sakura.ne.jp:www/mng/crud_base_l/dev/.env
+
+echo "------------ アップロード完了"
+#cmd /k
