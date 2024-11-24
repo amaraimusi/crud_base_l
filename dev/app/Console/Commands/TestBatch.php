438d37aaeac868e15848634cbfca1e326bf876e7
amaraimusi <amaraimusi@gmail.com>
2024-11-25 08:08:45 +0900

    テストバッチを修正

--- dev/app/Console/Commands/TestBatch.php
+++ dev/app/Console/Commands/TestBatch.php
@@ -3,31 +3,37 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate¥Support¥Facades¥DB; // DBを扱えるようにする
+use App\Models\Neko;
 
 class TestBatch extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'app:test-batch';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
-
-    /**
-     * Execute the console command.
-     */
-    public function handle()
-    {
-        		echo 'バッチ処理を開始します。';
-
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'batch:test';
+	
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Command description';
+	
+	/**
+	 * Execute the console command.
+	 */
+	public function handle()
+	{
+		echo 'バッチ処理を開始します。';
+		
+		$model = new Neko();
+		$ent = $model->find(77);
+		dump($ent);//■■■□□□■■■□□□)
+		
 		
 		echo 'バッチ処理を終了しました。';
-    }
+	}
 }
